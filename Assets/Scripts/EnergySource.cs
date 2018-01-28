74bc2cb5fb0fcc11eee8b8d337bc28ebaa0a11ff
Ori Hanegby <ori.hanegby@gmail.com>
2018-01-28 01:06:31 -0800

    added vr player

---
+++ Assets/Scripts/EnergySource.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnergySource : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger!!!");
+
+        ExecuteEvents.Execute<IMagicSourceEvent>(other.gameObject, null, (x, y) => x.MagicSourceEnergy());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ExecuteEvents.Execute<IMagicSourceEvent>(other.gameObject, null, (x, y) => x.MagicSourceEnergyExit());
+    }
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,21 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : MonoBehaviour, IMagicSourceEvent
+{
     public GameObject rightHand;
+    public GameObject leftHand;
     public GameObject probe;
+    public GameObject magicProbe;
+    public GameObject magicParticles;
+    public GameObject magicBeam;
+ 
     bool isThumbPressed = false;
+    bool magicMode = false;
     Vector3 thumbStart;
+    public float playerHeight = 10.0f;
 	// Use this for initialization
 	void Start () {
         
         
 	}
+
+    void updateMagicProbe()
+    {
+        Vector3 dirVec = leftHand.transform.position - this.transform.position;
+        float distance = -leftHand.transform.localRotation.x;
+
+        distance = Mathf.Max(distance, 5);
+        //distance = Mathf.Min(distance, 10);
+        //Debug.Log(distance);
+    
+        RaycastHit hit;
+
+        Vector3 forwardVector = leftHand.transform.rotation * Vector3.forward;
+
+        bool haveHit = false;
+        //probe.transform.position = rayStart + forwardVector * 20.0f;
+        if (Physics.Raycast(leftHand.transform.position, forwardVector, out hit))
+        {
+            haveHit = true;
+            magicProbe.transform.position = leftHand.transform.position + forwardVector * hit.distance * 0.9f;
+        }
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 touchAxis = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick) * Time.deltaTime;
+        if (magicMode)
+        {
+           
+            OVRInput.SetControllerVibration(3, 3, OVRInput.Controller.LTouch);
+            updateMagicProbe();
+        }
+        Vector2 axisVecs = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        Vector2 touchAxis = axisVecs * Time.deltaTime;
         if (touchAxis.x != 0)
         {
             if (!isThumbPressed)
@@ -25,21 +64,26 @@ public class PlayerControl : MonoBehaviour {
             }
            // Debug.Log(touchAxis.x);
             Vector3 dirVec = rightHand.transform.position - this.transform.position;
-            float distance = Mathf.Max(rightHand.transform.position.y - thumbStart.y, 0) * 50;
-            distance = Mathf.Max(distance, 3);
-            distance = Mathf.Min(distance, 10);
-            Debug.Log(distance);
-            Vector3 normVec = dirVec.normalized * 10;
-            Vector3 rayStart = normVec + this.transform.position;
+            float distance = -rightHand.transform.localRotation.x;
+           
+            distance = Mathf.Max(distance, 5);
+            //distance = Mathf.Min(distance, 10);
+            //Debug.Log(distance);
+            Vector3 normVec = dirVec.normalized * distance;
+            Vector3 rayStart = normVec + this.transform.position + new Vector3(0,200.0f,0);
+            
             RaycastHit hit;
 
-            if (Physics.Raycast(transform.position, dirVec.normalized, out hit))
+            Vector3 forwardVector = rightHand.transform.rotation * Vector3.forward;
+            Debug.Log(forwardVector);
+            bool haveHit = false;
+            rayStart = rightHand.transform.position;
+            //probe.transform.position = rayStart + forwardVector * 20.0f;
+            if (Physics.Raycast(rightHand.transform.position, forwardVector, out hit))
             {
-                probe.transform.position = probe.transform.position + dirVec.normalized * hit.distance;
-            } else if (Physics.Raycast(rayStart, -Vector3.up, out hit))
-            {
+                haveHit = true;
                 rayStart.y = rayStart.y - hit.distance;
-                probe.transform.position = rayStart;
+                probe.transform.position = rightHand.transform.position + forwardVector * hit.distance * 0.9f;
             }
         } else
         {
@@ -49,11 +93,29 @@ public class PlayerControl : MonoBehaviour {
                 Vector3 posFix = new Vector3(probe.transform.position.x, transform.position.y, probe.transform.position.z);
                 Vector3 relativePos = posFix - transform.position;
                 Quaternion rotation = Quaternion.LookRotation(relativePos);
-                transform.position = probe.transform.position;
+                transform.position = probe.transform.position + new Vector3(0, playerHeight,0);
                 transform.rotation = rotation;
                 
             }
         }
       //  transform.position += new Vector3(touchAxis.x, 0, touchAxis.y);
+    }
+
+    public void MagicSourceEnergy()
+    {
+        magicParticles.SetActive(true);
+        magicBeam.SetActive(true);
+        magicProbe.SetActive(true);
+        OVRInput.SetControllerVibration(10, 10, OVRInput.Controller.RTouch);
+        magicMode = true;
+    }
+
+    public void MagicSourceEnergyExit()
+    {
+        magicParticles.SetActive(false);
+        magicBeam.SetActive(false);
+        magicProbe.SetActive(false);
+        OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.LTouch);
+        magicMode = false;
     }
 }
